31b73cd6d9876376f12500836e30a18a5a8c669a
mariomac <mariomac@gmail.com>
2015-05-25 14:01:44 +0200

    Misc Stuff

--- powerModelling/prototype/src/main/java/es/bsc/autonomic/powermodeller/configuration/CoreConfiguration.java
+++ powerModelling/prototype/src/main/java/es/bsc/autonomic/powermodeller/configuration/CoreConfiguration.java
@@ -1,3 +1,18 @@
+/*
+    Copyright 2015 Barcelona Supercomputing Center
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 package es.bsc.autonomic.powermodeller.configuration;
 
 import org.apache.commons.configuration.ConfigurationException;
@@ -10,9 +25,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
-/**
- * Created by jsubirat on 14/11/14.
- */
 public class CoreConfiguration {
 
     public static char CSV_DELIMITER;
